a21cb349f3b08968fc39e4187a6967d4213e654a
Burningfoot <bufoot@gmail.com>
2021-11-13 15:53:26 +0100

    Setup Project

---
+++ Views/MainWindow.xaml.cs
@@ -0,0 +1,17 @@
+﻿using AutoKeyPress.ViewModels;
+using System.Windows;
+
+namespace AutoKeyPress
+{
+    /// <summary>
+    /// Interaktionslogik für MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            DataContext = new MainViewModel();
+            InitializeComponent();
+        }
+    }
+}
